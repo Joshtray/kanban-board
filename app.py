7d675914ccf072aabdd4a05a1a99336650e4bc79
Jessey Uche-Nwichi <jessey@uni.minerva.edu>
2023-05-07 23:13:12 -0400

    Added environment variables

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import os
 
 from flask import Flask
 from flaskr import create_app, db
+from dotenv import dotenv_values
 
 app = create_app()
+env = dotenv_values(".env")
 
 if __name__ == '__main__':
     with app.app_context():
         # If the DB file doesn't exist, make a new DB. 
         # Comment this line out if you want the DB to restart on every launch. 
-        if not os.path.exists(os.path.join(app.instance_path, "flaskr.sqlite")):
+        # if not os.environ.get('DATABASE_URL'):
             db.init_db()
     app.run(debug=True)
